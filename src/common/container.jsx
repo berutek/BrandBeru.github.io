fb2360632ad086424203e0fc0311cd1b90abd92c
Giovanny Bernal <brand.beru@gmail.com>
2023-11-10 18:42:31 -0500

    icon added and bugs fixed

--- src/common/container.jsx
+++ src/common/container.jsx
@@ -1,7 +1,7 @@
 export default function Container({children}){
   return (
     <>
-      <div className='min-w-full w-full max-h-screen py-14 lg:px-10 px-5'>
+      <div className='min-w-full w-full max-h-screen py-14 lg:p-10 p-5'>
         {children}
       </div>
     </>
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,13 +3,11 @@ import '@styles/globals.css'
 export default function MainLayout({ children }) {
   return (
     <>
-      <div className='min-h-full min-w-full flex justify-center items-center'>
-        <main className='w-full'>
-          <div className='max-w-7xl'>
-            {children}
-          </div>
+      <div className="min-h-full min-w-full flex justify-center items-center">
+        <main className="w-full">
+          <div className="max-w-7xl">{children}</div>
         </main>
       </div>
     </>
-  )
+  );
 }
