ae57f74f49f39710f5f7a0345cf35537d1eb06e8
Giovanny Bernal <118768050+giobero1402@users.noreply.github.com>
2025-02-20 10:44:41 -0500

    Update index.js

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,7 +1,7 @@
 export const endPoints = {
   github: {
-    BrandBeru: 'https://api.github.com/users/BrandBeru/repos',
-    user: 'https://api.github.com/users/BrandBeru',
+    BrandBeru: 'https://api.github.com/users/giobero1402/repos',
+    user: 'https://api.github.com/users/giobero1402',
     pnova: 'https://api.github.com/users/PNOVA-VIGE-STUDIOS',
     'PNOVA-VIGE-STUDIOS': 'https://api.github.com/orgs/PNOVA-VIGE-STUDIOS/repos',
     migion: 'https://api.github.com/users/Migion0506',
